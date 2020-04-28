c956074a594c9b972403b65c2b6032d60a6655fa
Caio Silva <caiopsilva1@gmail.com>
2020-04-28 19:25:36 -0300

    feat: Finishing chat rooms

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,24 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AppGateway } from './chat/app.gateway';
+import { UserModule } from './user/user.module';
+import { UserTypeModule } from './usertype/usertype.module';
+import { RegionModule } from './region/region.module';
 import { PublicGroupModule } from './public-group/public-group.module';
 import { GroupModule } from './group/group.module';
+import { AppGateway } from './chat/app.gateway';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(),
     UserModule,
     UserTypeModule,
     RegionModule,
     PublicGroupModule,
-    GroupModule
+    GroupModule,
   ],
   controllers: [AppController],
   providers: [AppService, AppGateway],
 })
 export class AppModule {}
-

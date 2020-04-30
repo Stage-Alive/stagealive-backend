57508b7b8d568cd30be5c12fd09b0c36596aac62
Caio Silva <caiopsilva1@gmail.com>
2020-04-30 17:13:58 -0300

    Fixing format

--- src/dtos-global/index-query.dto.ts
+++ src/dtos-global/index-query.dto.ts
@@ -6,12 +6,20 @@ export class IndexQueryDto {
   @IsOptional()
   @Type(() => Number)
   @Min(1)
-  @ApiProperty({ description: 'The current page of pagination', default: 1, minimum: 1 })
+  @ApiProperty({
+    description: 'The current page of pagination',
+    default: 1,
+    minimum: 1,
+  })
   page: number;
 
   @IsOptional()
   @Type(() => Number)
   @Min(1)
-  @ApiProperty({ description: 'Number of items per page', default: 10, minimum: 1 })
+  @ApiProperty({
+    description: 'Number of items per page',
+    default: 10,
+    minimum: 1,
+  })
   limit: number;
 }

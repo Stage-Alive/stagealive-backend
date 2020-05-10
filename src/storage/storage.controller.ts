c5deb36728d7edf18aa95a18bc581630a6e7cf75
Endhe Soares <endhe.soares@sebsa.com.br>
2020-05-09 23:30:24 -0300

    feat: adding routes

--- src/storage/storage.controller.ts
+++ src/storage/storage.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Req } from '@nestjs/common';
+import { StorageService } from './storage.service';
+import { SignUrlDto } from './dto/sign-url.dto';
+import { ApiTags } from '@nestjs/swagger';
 
-@Controller('storage')
-export class StorageController {}
+@Controller('storages')
+@ApiTags('storages')
+export class StorageController {
+  constructor(private readonly storageService: StorageService) {}
+
+  @Post()
+  async getSignURL(@Body() data: SignUrlDto, @Req() req) {
+    const result = this.storageService.getSignURL(data);
+    return {
+      message: 'Get a sign url for put S3 -- expires 1 hour',
+      object: 'string',
+      url: req.url,
+      data: result,
+    };
+  }
+}
